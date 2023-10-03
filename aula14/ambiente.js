f1bb2a3d65e0c2036628f6fa67b0a70544d37f9a
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-10-03 05:13:38 -0300

    exercícios

---
+++ aula14/ambiente.js
@@ -0,0 +1,20 @@
+/* var c = 1
+while (c <= 5){
+  console.log(c)
+  c++ // formas de escrever comando ao lado: c+ = 1 outra c = c + 1
+} */
+
+/* Abaixo com do while
+var c = 1
+ do {
+  console.log(c)
+  c++ 
+}while (c <= 5) */
+
+// Abaixo com for
+console.log("Vai começar...")
+
+for (var c = 1; c <= 5; c++) {
+  console.log(c)
+}
+console.log("FIM")
