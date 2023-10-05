f12a88d40cce5ef6c8dd0c84bbb43ac2d6fd0171
florianopolismarcelo <florianopolismarcelo@msn.com>
2023-10-05 12:07:30 -0300

    exercícios

---
+++ aula16/funcao01.js
@@ -0,0 +1,8 @@
+function parimpar(n) {
+  if (n % 2 == 0) {
+    return "O número selecionado é PAR"
+  } else {
+    return "O número selecionado é IMPAR"
+  }
+}
+console.log(parimpar(222))
---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1 = 0, n2 = 0) {
+  // O =0 é se não colocar abaixo o n1 ou n2 exemplo: console.log(soma(7))
+  return n1 + n2
+}
+console.log(soma(7, 3))
